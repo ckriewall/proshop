3074560d13017765a68dbec839460cb60a936ad9
Charlie Kriewall <charlie@kriewall.com>
2020-10-27 17:30:53 -0700

    add bootstrap, Header/Footer. post-vid 2.5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to Proshop</h1>
+      <Header />
+      {/* Create space between the header and main body with y-axis padding */}
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
