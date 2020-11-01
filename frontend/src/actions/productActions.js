2d95eb96d70e54ac572a8d98efaad8ddffac59ba
Charlie Kriewall <charlie@kriewall.com>
2020-11-01 11:30:49 -0800

    add comments to productActions for clarity

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,3 +1,16 @@
+/*
+Action creators are functions that create actions.
+
+The listProducts() action creator:
+  1. Wraps actions in a try/catch block for error handling.
+  2. Dispatches a products request to the productsReducer.
+  3. Requests products from the API/Mongo.
+  4. Passes returned data to the productsReducer on success.
+  5. Passes returned errors to the productsReducer on failure.
+  
+Calling an async dispatch function within the listProducts 
+action creator is possible because of redux-thunk.
+*/
 import axios from 'axios';
 import {
   PRODUCT_LIST_REQUEST,
@@ -7,10 +20,11 @@ import {
 
 export const listProducts = () => async (dispatch) => {
   try {
-    dispatch({ type: PRODUCT_LIST_REQUEST });
+    dispatch({
+      type: PRODUCT_LIST_REQUEST,
+    });
 
     const { data } = await axios.get('/api/products');
-
     dispatch({
       type: PRODUCT_LIST_SUCCESS,
       payload: data,
