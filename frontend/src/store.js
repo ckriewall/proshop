26f33d4355b225e51a3c94188db5249f10d5a335
Charlie Kriewall <charlie@kriewall.com>
2020-11-01 09:32:12 -0800

    add Redux store. post-vid 5.26

---
+++ frontend/src/store.js
@@ -0,0 +1,30 @@
+/*
+createStore: Creates a Redux store that holds the state tree. 
+    he only way to change the data in the store is to call 
+    dispatch() on it.
+
+combineReducers: Turns an object whose values are different 
+    reducer functions, into a single reducer function. It will 
+    call every child reducer, and gather their results into a 
+    single state object, whose keys correspond to the keys of 
+    the passed reducer functions.
+
+applyMiddleware: a store enhancer that applies middleware 
+    to the dispatch method of the Redux store
+*/
+
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const reducer = combineReducers({});
+const initialState = {};
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
