564f796a2a0720f68e9679c657af2daa4bbd4398
Charlie Kriewall <charlie@kriewall.com>
2020-11-02 13:46:29 -0800

    add Add to Cart functionality. post-vid 6.33

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Project for [MERN eCommerce from Scratch](https://www.udemy.com/course/mern-ecom
 ## To-Do
 
 1. Display an error message on error in `/src/actions/cartActions.js` => `addToCart()`
+1. Gracefully retrieve qty from URL in `/src/screens/CartScreen.js`
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -17,7 +17,7 @@ import { listProductDetails } from '../actions/productActions';
 
 const ProductScreen = ({ history, match }) => {
   // component state
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
 
   // Redux global state
   const dispatch = useDispatch();
