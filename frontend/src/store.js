ca7ffd669bcbd4e96579de05c61ce511458aabc0
Charlie Kriewall <charlie@kriewall.com>
2020-11-01 16:20:29 -0800

    add productDetails reducer + action. post-vid 5.30

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -17,8 +17,14 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import { productListReducer } from './reducers/productReducers';
-const reducer = combineReducers({ productList: productListReducer });
+import {
+  productDetailsReducer,
+  productListReducer,
+} from './reducers/productReducers';
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+});
 const initialState = {};
 const middleware = [thunk];
 
